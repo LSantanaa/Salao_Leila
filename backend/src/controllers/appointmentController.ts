a5a2c5f813129d37d9d3a86a02c7e22e73042f99
Leonardo <leonardodesantana05@gmail.com>
2025-02-28 20:36:29 -0300

    chore: ajustes de nome de variaveis

--- backend/src/controllers/appointmentController.ts
+++ backend/src/controllers/appointmentController.ts
@@ -2,9 +2,13 @@ import { Request, Response } from "express";
 import {
   createNewAppointment,
   fetchAllAppointments,
-  fetchClientAppointments,
+  fetchUserAppointments,
 } from "../services/appointmentServices";
 import { asyncHandler } from "../lib/asyncHandler";
+
+interface AuthRequest extends Request {
+  user?: { id: number; role: string };
+}
 
 export const getAllAppointments = asyncHandler(
   async (req: Request, res: Response) => {
@@ -13,25 +17,27 @@ export const getAllAppointments = asyncHandler(
   }
 );
 
-export const getClientAppointments = asyncHandler(
-  async (req: Request, res: Response) => {
-    const { clientId } = req.body;
-    const clientAppointments = await fetchClientAppointments(clientId);
+export const getUserAppointments = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const  userId  = req.user!.id;
+    const clientAppointments = await fetchUserAppointments(userId);
     res.json(clientAppointments);
   }
 );
 
-export const createAppointment = asyncHandler(
-  async (req: Request, res: Response) => {
-    const { clientId, serviceId, timeStamp, useSuggestion } = req.body;
 
-    if (!clientId || !serviceId || !timeStamp) {
+export const createAppointment = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const { serviceId, timeStamp, useSuggestion } = req.body;
+    const userId = req.user!.id
+
+    if (!userId || !serviceId || !timeStamp) {
       res.status(400);
       throw new Error("Campos devem ser obrigatórios");
     }
 
     const result = await createNewAppointment({
-      clientId,
+      userId,
       serviceId,
       timeStamp,
       useSuggestion,
