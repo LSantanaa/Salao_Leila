4c7b830a50fb8a9153983a9b158adbec5c2988d9
Leonardo <leonardodesantana05@gmail.com>
2025-02-28 20:22:36 -0300

    feat: serviços relacionados a autenticação e criação de usuários

---
+++ backend/src/services/authService.ts
@@ -0,0 +1,35 @@
+import prisma from '../lib/prisma';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = 'salao-leila'; 
+
+type Role = 'admin' | 'client';
+
+/**
+ * Cria um usuário com senha criptografada.
+ * @param name - nome do usuário
+ * @param email - Email único.
+ * @param password - Senha em texto plano.
+ * @param role - "admin" ou "client".
+ */
+export const createUser = async (name:string, email: string, password: string, role: Role) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return prisma.user.create({
+    data: { name,email,pwd: hashedPassword, role },
+  });
+};
+
+/**
+ * Autentica um usuário e retorna um token JWT.
+ * @param email - Email do usuário.
+ * @param password - Senha em texto plano.
+ */
+export const login = async (email: string, password: string) => {
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user || !(await bcrypt.compare(password, user.pwd))) {
+    throw new Error('Credenciais inválidas');
+  }
+  const token = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
+  return { token, role: user.role };
+};
