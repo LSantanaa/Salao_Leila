d87909ff6c1aa05fe4d37736c241bf6c60598c75
Leonardo <leonardodesantana05@gmail.com>
2025-03-01 15:45:01 -0300

    ajustes de comentários

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -14,16 +14,25 @@ type Role = 'admin' | 'client';
  * @param role - "admin" ou "client".
  */
 export const createUser = async (name:string, email: string, password: string, role: Role) => {
-  const hashedPassword = await bcrypt.hash(password, 10);
-  return prisma.user.create({
-    data: { name,email,pwd: hashedPassword, role },
-  });
+  const emailExisting = await prisma.user.findUnique({where: {email}})
+
+  if(!emailExisting){
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    return prisma.user.create({
+      data: { name,email,pwd: hashedPassword, role },
+    });
+  }else{
+    throw new Error("Email já cadastrado")
+  }
 };
 
 /**
  * Autentica um usuário e retorna um token JWT.
  * @param email - Email do usuário.
  * @param password - Senha em texto plano.
+ * @throws {Error} - se login for inválido
+ * @returns - um objeto contendo o token de acesso e o tipo de usuário
  */
 export const userLogin = async (email: string, password: string) => {
   const user = await prisma.user.findUnique({ where: { email } });
