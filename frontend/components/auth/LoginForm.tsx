f9172f7150e1acbefceaa0f89ad52ca458b9f531
Leonardo <leonardodesantana05@gmail.com>
2025-03-02 16:37:48 -0300

    componentes referentes aos formulários

--- frontend/components/auth/LoginForm.tsx
+++ frontend/components/auth/LoginForm.tsx
@@ -1,17 +1,35 @@
 "use client";
-import { useState } from "react";
+import {  useState } from "react";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
+import { useAuth } from "@/providers/authProvider";
+
 
 export default function LoginForm({ toggleForm }: { toggleForm: () => void }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const { login } = useAuth();
+
+  
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      await login(email, password);
+    } catch (error) {
+      setError((error as Error).message);
+      console.error("Erro ao fazer login: ", error);
+    }
+  };
 
   return (
-    <form className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4">
       <Input
         type="email"
         label="E-mail"
+        required
         placeholder="Digite seu e-mail"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
@@ -19,14 +37,21 @@ export default function LoginForm({ toggleForm }: { toggleForm: () => void }) {
       <Input
         type="password"
         label="Senha"
+        required
         placeholder="Digite sua senha"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
+      {error && (
+        <p className="text-sm my-2 text-red-600">{error}</p> // Exibe o erro abaixo dos campos
+      )}
       <Button text="Entrar" type="submit" />
       <p className="text-center text-sm text-gray-600">
         Não tem uma conta?{" "}
-        <button onClick={toggleForm} className="text-pink-500 font-semibold hover:underline">
+        <button
+          onClick={toggleForm}
+          className="text-pink-500 font-semibold hover:underline"
+        >
           Cadastre-se
         </button>
       </p>
--- frontend/components/auth/RegisterForm.tsx
+++ frontend/components/auth/RegisterForm.tsx
@@ -2,19 +2,36 @@
 import { useState } from "react";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
+import { useAuth } from "@/providers/authProvider";
 
 export default function RegisterForm({ toggleForm }: { toggleForm: () => void }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+   const [error, setError] = useState<string | null>(null);
+    const {register} = useAuth();
+  
+    const handleSubmit = async(e:React.FormEvent)=>{
+      e.preventDefault();
+      setError(null)
+      try {
+        await register(name,email, password)
+      } catch (error) {
+        setError((error as Error).message)
+        console.error("Erro ao fazer registro: ",error)
+      }
+    }
+    
+
   return (
-    <form className="space-y-4">
+    <form className="space-y-4" onSubmit={handleSubmit}>
       <Input
         type="text"
         label="Nome"
         placeholder="Digite seu nome"
         value={name}
+        required
         onChange={(e) => setName(e.target.value)}
       />
       <Input
@@ -22,6 +39,7 @@ export default function RegisterForm({ toggleForm }: { toggleForm: () => void })
         label="E-mail"
         placeholder="Digite seu e-mail"
         value={email}
+        required
         onChange={(e) => setEmail(e.target.value)}
       />
       <Input
@@ -29,8 +47,12 @@ export default function RegisterForm({ toggleForm }: { toggleForm: () => void })
         label="Senha"
         placeholder="Crie uma senha"
         value={password}
+        required
         onChange={(e) => setPassword(e.target.value)}
       />
+      {error && (
+          <p className="text-sm my-2 text-red-600">{error}</p> // Exibe o erro abaixo dos campos
+        )}
       <Button text="Cadastrar" type="submit" />
       <p className="text-center text-sm text-gray-600">
         Já tem uma conta?{" "}
