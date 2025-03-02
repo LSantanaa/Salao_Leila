f832dee2ccbef704f5dd9c2de52ea80c30a8288c
Leonardo <leonardodesantana05@gmail.com>
2025-03-02 00:01:29 -0300

    :wrench: ci: ajustes no retorno das funções (passando objeto json)

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -15,30 +15,25 @@ type Role = 'admin' | 'client';
  */
 export const createUser = async (name:string, email: string, password: string, role: Role) => {
   const emailExisting = await prisma.user.findUnique({where: {email}})
-
-  if(!emailExisting){
-    const hashedPassword = await bcrypt.hash(password, 10);
-  
-    return prisma.user.create({
-      data: { name,email,pwd: hashedPassword, role },
-    });
-  }else{
-    throw new Error("Email já cadastrado")
+  if(emailExisting){
+    return {success: false, error:"Email já cadastrado."}
   }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({data: { name,email,pwd: hashedPassword, role }, });
+    return {success: true, data: user}
 };
 
 /**
  * Autentica um usuário e retorna um token JWT.
  * @param email - Email do usuário.
  * @param password - Senha em texto plano.
- * @throws {Error} - se login for inválido
  * @returns - um objeto contendo o token de acesso e o tipo de usuário
  */
 export const userLogin = async (email: string, password: string) => {
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user || !(await bcrypt.compare(password, user.pwd))) {
-    throw new Error('Credenciais inválidas');
+    return {success: false, error: "Credenciais inválidas"};
   }
-  const token = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
-  return { token, role: user.role };
+  const token = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, { expiresIn: '2h' });
+  return { success: true, data:{token, role: user.role} };
 };
