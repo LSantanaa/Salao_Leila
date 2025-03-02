3ce0412ef5999f371637cd8bdeab077e488b44b8
Leonardo <leonardodesantana05@gmail.com>
2025-03-02 07:16:08 -0300

    add seed para um cliente ficticio

--- backend/prisma/db/seed.ts
+++ backend/prisma/db/seed.ts
@@ -4,33 +4,54 @@ import bcrypt from 'bcryptjs';
 const prisma = new PrismaClient();
 
 async function seed() {
-  const name = "Leila";
-  const email = 'admin@salao.com';
-  const password = 'admin123';
-  const hashedPassword = await bcrypt.hash(password, 10);
+  // Admin
+  const adminName = "Leila";
+  const adminEmail = 'admin@salao.com';
+  const adminPassword = 'admin123';
+  const adminHashedPassword = await bcrypt.hash(adminPassword, 10);
 
   const admin = await prisma.user.upsert({
-    where: { email },
+    where: { email: adminEmail },
     update: {},
     create: {
-      name,
-      email,
-      pwd: hashedPassword,
+      name: adminName,
+      email: adminEmail,
+      pwd: adminHashedPassword,
       role: 'admin',
     },
   });
 
+  // Cliente
+  const clientName = "Karen";
+  const clientEmail = "karen@cliente.com";
+  const clientPassword = "cliente123";
+  const clientHashedPassword = await bcrypt.hash(clientPassword, 10);
+
+  const client = await prisma.user.upsert({
+    where: { email: clientEmail },
+    update: {},
+    create: {
+      name: clientName,
+      email: clientEmail,
+      pwd: clientHashedPassword,
+      role: 'client',
+    },
+  });
+
+  // Serviços
   const services = await prisma.service.createMany({
-    data:[
-      {name: "Corte Simples", price: 50.00},
-      {name: "Corte especial", price: 70.00},
-      {name: "Tintura", price: 90.00},
-      {name: "Progressiva", price: 130.00},
-      {name: "Hidratação", price: 50.00},
-    ]
-  })
+    data: [
+      { name: "Corte Simples", price: 50.00, },
+      { name: "Corte Especial", price: 70.00, },
+      { name: "Tintura", price: 90.00, },
+      { name: "Progressiva", price: 130.00, },
+      { name: "Hidratação", price: 50.00},
+    ],
+    
+  });
 
   console.log('Admin criado:', admin);
+  console.log('Cliente criado:', client);
   console.log('Serviços criados:', services);
 }
 
